0f0b870094b9bd550072b58fa91da8dc3fc8e690
pkhatkar1 <priya.k.khatkar@gmail.com>
2024-03-05 03:47:21 -0800

    Redirect home page to login and messages

--- lyscoris/src/App.js
+++ lyscoris/src/App.js
@@ -1,16 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Home from './pages';
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages";
 import Authenticate from './pages/login';
 import MessagesPage from './pages/messages';
 
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//import Home from './Home';
+//import About from './About';
+//import Contact from './Contact';
+
 function App() {
   return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/pages" element={<Home/>} />
+          <Route path="/pages/login" element={<Authenticate/>} />
+          <Route path="/pages/messages" element={<MessagesPage/>} />
+        </Routes>
+    </BrowserRouter>
+  );
+};
+
+/*function App() {
+  return (
     <div>
-      <MessagesPage />
+      <Authenticate />
     </div>
   );
-}
+}*/
 
 export default App;
